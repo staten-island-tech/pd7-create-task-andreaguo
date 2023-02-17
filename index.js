f9f5dc4aa5425d1478e807e48feb982ccba4d1e6
andreaguo <andrea.guo2007@gmail.com>
2023-02-17 14:01:09 -0500

    asdf

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import { cards } from "../array.js";
+import { cards } from "./array.js";
 
-let newCards = []
-
-function random(min, max) {
-    let mino = Math.floor(min); //rounds down
-    let maxo = Math.ceil(max); //rounds up
-    return Math.floor(Math.random() * (maxo-mino+1)) + mino
-}
-// cards.sort((a,b) => 0.5 - Math.random())
-random(0,10);
-for (i = cards.length -1; i > 0; i--) {
-    j = Math.floor(Math.random() * i)
-    k = points[i]
+for (let i = cards.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i)
+    let k = cards[i]
     cards[i] = cards[j]
     cards[j] = k
   }
-console.log(cards)
+console.log(cards);
+
+function random(min, max) {
+    let MIN = Math.floor(min); //rounds down
+    let MAX = Math.ceil(max); //rounds up
+    return Math.floor(Math.random() * (MAX-MIN+1)) + MIN
+}
+let index = random(0, 51)
+let card = cards[index];
+console.log(card);
+
+if ( card.number >21) {
+  console.log("you lost!")
+} else {
+  console.log("another hit?")
+}
